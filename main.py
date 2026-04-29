54e9f9500d86d18ddc8836f9bfc45df226b2d217
nkakhidze <nick200069@gmail.com>
2026-04-29 16:20:20 +0700

    Add users endpoint

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ def hello():
     return {
         "message": "Hello, FastAPI!"
     }
+
+@app.get("/users")
+def get_users():
+    return [
+        {
+            "id": 1,
+            "email": "test@test.com",
+            "name": "Nikolay"
+        },
+        {
+            "id": 2,
+            "email": "admin@test.com",
+            "name": "Admin"
+        }
+    ]
